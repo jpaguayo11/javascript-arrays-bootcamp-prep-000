eee54cba64dce150a6a9127484960a24de3d0f9b
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-03 01:05:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,5 +24,6 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift
+  array.shift
+  return array
 }
