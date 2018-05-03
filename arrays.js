a0a5cc06d30c04937937f442f4ec0bfff72cb70a
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-03 01:09:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice(1)
+  array.slice(1)
   return array
 }
